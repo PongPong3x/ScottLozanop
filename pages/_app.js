cca51e7b13e54bcf8cf032d9cb54778d07ae16bc
PongPong3x <xfnyk113s@sina.com>
2021-04-12 09:10:07 +0800

    finalize text size changes

--- pages/_app.js
+++ pages/_app.js
@@ -35,22 +35,22 @@ const connectWalletPressed = async () => {
         <div className="flex mt-4" >
         
           <Link href="/">
-            <a className="mr-4 text-blue-500 text-3xl">
+            <a className="mr-4 text-blue-500 text-2xl">
               Marketplace
             </a>
           </Link>
           <Link href="/my-assets">
-            <a className="mr-6 text-blue-500 text-3xl">
+            <a className="mr-6 text-blue-500 text-2xl">
               NFTs Owned
             </a>
           </Link>
           <Link href="/create-item">
-            <a className="mr-6 text-blue-500">
+            <a className="mr-6 text-blue-500 text-sm">
               Jaleh Creation Studio
             </a>
           </Link>
           <Link href="/creator-dashboard">
-            <a className="mr-6 text-blue-500">
+            <a className="mr-6 text-blue-500 text-sm">
               AOJ Dashboard
             </a>
           </Link>
